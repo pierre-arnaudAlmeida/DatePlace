afaf952ebb0a4b9907dad8da02afed614957c997
pierre-arnaudAlmeida <37550509+pierre-arnaudAlmeida@users.noreply.github.com>
2019-02-01 01:00:35 +0100

    v0.0.7

--- app/src/main/java/fr/blackmamba/dateplaceapp/ConnexionActivity.java
+++ app/src/main/java/fr/blackmamba/dateplaceapp/ConnexionActivity.java
@@ -1,31 +1,45 @@
 package fr.blackmamba.dateplaceapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.android.volley.Response;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.blackmamba.dateplaceapp.backgroundtask.ServiceHandler;
 
 
 public class ConnexionActivity extends AppCompatActivity {
     private Button button_goback;
     private Button button_connect;
+    EditText email,password;
+    String urlAdd = "https://dateplaceapp.000webhostapp.com/login.php";
+    ConnexionActivity.GetDataAsyncTask GetData;
+    String message;
+    int success;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
-        setTitle("Connexion");
 
-        final EditText new_email = (EditText) findViewById(R.id.connexion_identifier);
-        final EditText new_password = (EditText) findViewById(R.id.connexion_password);
+        email = (EditText) findViewById(R.id.connexion_identifier);
+        password = (EditText) findViewById(R.id.connexion_password);
 
         this.button_goback=findViewById(R.id.button_goback);
         button_goback.setOnClickListener(new View.OnClickListener() {
@@ -40,34 +54,80 @@ public class ConnexionActivity extends AppCompatActivity {
         button_connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String email = new_email.getText().toString();
-                final String password = new_password.getText().toString();
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success){
-                                Intent connect = new Intent(getApplicationContext(),UserProfilActivity.class);
-                                startActivity(connect);
-                                finish();
-                            }else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(ConnexionActivity.this);
-                                builder.setMessage("Login Failed")
-                                        .setNegativeButton("Retry", null)
-                                        .create()
-                                        .show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                if ( email.getText().toString().equals("") || password.getText().toString().equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ConnexionActivity.this);
+                    builder.setMessage("Il manque mot de passe/email")
+                            .setNegativeButton("Retry",null)
+                            .create()
+                            .show();
+                }else{
+                    int testemail = email.getText().toString().indexOf("@");
+                    if (testemail != -1) {
+                        GetData = new GetDataAsyncTask();
+                        GetData.execute();
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ConnexionActivity.this);
+                        builder.setMessage("Vous n'avez pas entrer une adresse email :)")
+                                .setNegativeButton("Retry",null)
+                                .create()
+                                .show();
                     }
-                };
-                //Intent connect = new Intent(getApplicationContext(),UserProfilActivity.class);
-                //startActivity(connect);
-                //finish();
+                }
             }
         });
+    }
+
+    public class GetDataAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.i("add", " start doInBackground");
+
+            ServiceHandler sh = new ServiceHandler();
+
+            List<NameValuePair> nameValuePair = new ArrayList<>(1);
+
+            nameValuePair.add(new BasicNameValuePair("email", email.getText().toString()));
+            nameValuePair.add(new BasicNameValuePair("password", password.getText().toString()));
+
+            String jsonStr = sh.makeServiceCall(urlAdd, ServiceHandler.POST, nameValuePair);
+
+            Log.d("Response: ", jsonStr);
+            if (jsonStr != null) {
+                try {
+
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    success = jsonObj.getInt("success");
+                    message = jsonObj.getString("message");
+                    Log.i("success", String.valueOf(success));
+                    Log.i("message", message);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.i("add", " end doInBackground");
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+            if (success==1){
+                Intent gobefore = new Intent(ConnexionActivity.this, MapActivity.class);
+                startActivityForResult(gobefore,100);
+                finish();
+            }else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ConnexionActivity.this);
+                builder.setMessage("Connexion Failed");
+                builder.setNegativeButton("Retry", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent goback = new Intent(ConnexionActivity.this, ConnexionActivity.class);
+                        startActivityForResult(goback,100);
+                        finish();
+                    }
+                });
+                builder.show();
+            }
+        }
     }
 }
--- app/src/main/java/fr/blackmamba/dateplaceapp/InscriptionActivity.java
+++ app/src/main/java/fr/blackmamba/dateplaceapp/InscriptionActivity.java
@@ -1,7 +1,10 @@
 package fr.blackmamba.dateplaceapp;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -11,9 +14,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
@@ -22,37 +26,39 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import fr.blackmamba.dateplaceapp.backgroundtask.ServiceHandler;
 
-public class InscriptionActivity extends AppCompatActivity {
+public class InscriptionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private Button button_goback;
     private Button button_inscription;
-    EditText name, last_name, password, email, date_de_naissance;
-    String urlAdd = "https://dateplaceapp.000webhostapp.com/insert.php";
+    EditText name,last_name,password,email;
+    Button date_de_naissance;
+    private DatePickerDialog.OnDateSetListener date_de_naissance_Listener;
+    Spinner but;
+    String urlAdd = "https://dateplaceapp.000webhostapp.com/insert_user.php";
     AddDataAsyncTask AddData;
-    String message;
-    int success;
+    String message,birthday;
+    int success, year,month,day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
 
-//        Spinner goal = findViewById(R.id.newgoal);
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.goal, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        goal.setAdapter(adapter);
-//        goal.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        Spinner goal = findViewById(R.id.newgoal);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.goal, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        goal.setAdapter(adapter);
+        goal.setOnItemSelectedListener(this);
 
         name = (EditText) findViewById(R.id.new_prenom);
         last_name = (EditText) findViewById(R.id.new_nom);
         password = (EditText) findViewById(R.id.newmot_de_passe);
         email = (EditText) findViewById(R.id.newadressemail);
-        date_de_naissance = (EditText) findViewById(R.id.newdate_de_naissance);
-        //but = (EditText) findViewById(R.id.newgoal);
-        AddData = new AddDataAsyncTask();
+        but = (Spinner) findViewById(R.id.newgoal);
 
         this.button_goback = findViewById(R.id.button_goback);
         button_goback.setOnClickListener(new View.OnClickListener() {
@@ -64,25 +70,68 @@ public class InscriptionActivity extends AppCompatActivity {
             }
         });
 
+        this.date_de_naissance = (Button) findViewById(R.id.newdate_de_naissance);
+        date_de_naissance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datedialog = new DatePickerDialog(
+                        InscriptionActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        date_de_naissance_Listener,
+                        year,month,day);
+                datedialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datedialog.show();
+            }
+        });
+        date_de_naissance_Listener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker,int year,int month,int day){
+                month=month+1;
+                birthday = ""+year+"/"+month+"/"+day;
+            }
+        };
+
         this.button_inscription = findViewById(R.id.button_inscrip);
         button_inscription.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( name.getText().toString().equals("") || last_name.getText().toString().equals("") || password.getText().toString().equals("") || email.getText().toString().equals("") || date_de_naissance.getText().toString().equals("")){
+                if ( name.getText().toString().equals("") || last_name.getText().toString().equals("") || password.getText().toString().equals("") || email.getText().toString().equals("")){
                     AlertDialog.Builder builder = new AlertDialog.Builder(InscriptionActivity.this);
-                   builder.setMessage("Inscription Failed")
+                   builder.setMessage("Les champs ne sont pas tous plein")
                            .setNegativeButton("Retry",null)
                            .create()
                            .show();
                 }else{
+                    int testemail = email.getText().toString().indexOf("@");
+                    if (testemail != -1) {
                     AddData = new AddDataAsyncTask();
                     AddData.execute();
-                    Intent gobefore = new Intent(getApplicationContext(), ConnexionActivity.class);
-                    startActivity(gobefore);
-                    finish();
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(InscriptionActivity.this);
+                        builder.setMessage("Vous n'avez pas entrer une adresse email :)")
+                                .setNegativeButton("Retry",null)
+                                .create()
+                                .show();
+                    }
                 }
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     public class AddDataAsyncTask extends AsyncTask<Void, Void, Void> {
@@ -90,7 +139,7 @@ public class InscriptionActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             Log.i("add", " start doInBackground");
-            // Creating service handler class instance
+
             ServiceHandler sh = new ServiceHandler();
 
             List<NameValuePair> nameValuePair = new ArrayList<>(1);
@@ -99,8 +148,8 @@ public class InscriptionActivity extends AppCompatActivity {
             nameValuePair.add(new BasicNameValuePair("last_name", last_name.getText().toString()));
             nameValuePair.add(new BasicNameValuePair("password", password.getText().toString()));
             nameValuePair.add(new BasicNameValuePair("email", email.getText().toString()));
-            nameValuePair.add(new BasicNameValuePair("date_de_naissance", date_de_naissance.getText().toString()));
-            //nameValuePair.add(new BasicNameValuePair("goal", but.getText().toString()));
+            nameValuePair.add(new BasicNameValuePair("date_de_naissance", birthday));
+            nameValuePair.add(new BasicNameValuePair("goal", but.getSelectedItem().toString()));
 
             String jsonStr = sh.makeServiceCall(urlAdd, ServiceHandler.POST, nameValuePair);
 
@@ -120,6 +169,26 @@ public class InscriptionActivity extends AppCompatActivity {
             Log.i("add", " end doInBackground");
             return null;
         }
+        @Override
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+               if (success==1){
+                   Intent gobefore = new Intent(InscriptionActivity.this, ConnexionActivity.class);
+                   startActivityForResult(gobefore,100);
+                   finish();
+               }else {
+                   AlertDialog.Builder builder = new AlertDialog.Builder(InscriptionActivity.this);
+                   builder.setMessage("Inscription Failed");
+                   builder.setNegativeButton("Retry", new DialogInterface.OnClickListener() {
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                            Intent goback = new Intent(InscriptionActivity.this, InscriptionActivity.class);
+                            startActivityForResult(goback,100);
+                            finish();
+                       }
+                   });
+                   builder.show();
+               }
+        }
     }
 }
-
